4add6660ee5b523fb405c7c43b11287073682027
Dhruv-Dugar <dhruvdugar02@gmail.com>
2024-11-20 06:21:48 +0000

    added main app js business logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ViewTransaction from './pages/ViewTransaction';
+import ViewUniqueTransaction from './pages/ViewUniqueTransaction';
+import CreateTransaction from './pages/CreateTransaction';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter className="App">
+      <Navbar />
+      <Routes>
+        <Route path='/viewtransactions' element={<ViewTransaction />} />
+        <Route path='/viewuniquetransactions' element={<ViewUniqueTransaction />} />
+        <Route path='/createtransaction' element={<CreateTransaction />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,5 +18,4 @@ function Navbar() {
     )
 }
 
-
 export default Navbar;
