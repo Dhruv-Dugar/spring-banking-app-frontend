485a848b05ec3eb8f372e6585176fbb93869ac6b
Vinayak-kul11-11 <vinayakkulshrestha2003@outlook.com>
2024-11-20 12:27:21 +0000

    Frontend

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Navbar from './components/Navbar';
 import ViewTransaction from './pages/ViewTransaction';
 // import ViewUniqueTransaction from './pages/ViewUniqueTransaction';
 import CreateTransaction from './pages/CreateTransaction';
-
+import CustDeets from './pages/ViewCustomer';
+import UpdateCustomerDetails from './pages/UpdateCustomer';
 
 function App() {
   return (
     <BrowserRouter className="App">
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path='/viewtransactions' element={<ViewTransaction />} />
         {/* <Route path='/viewuniquetransactions' element={<ViewUniqueTransaction />} /> */}
         <Route path='/createtransaction' element={<CreateTransaction />} />
+        <Route path="/viewcustomers" element={<CustDeets />} />
+        <Route path="/UpdateCustomer" element={<UpdateCustomerDetails />} />
       </Routes>
     </BrowserRouter>
   );
